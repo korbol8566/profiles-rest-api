5a313bbab3991e56b586d134da7a324a7b5dddc6
Boldizsar <boldizsar.kormendy@gmail.com>
2022-05-30 18:41:51 +0200

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,56 +1,58 @@
-from django.db                      import models
-from django.contrib.auth.models     import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
-    """Manager for User profiles"""
-    
-    def create_user(self,email, name, password=None):
-        """Create a new User profile"""
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError("Please provide an email address")
-        
-        email=self.normalize_email(email)
-        user=self.create_user(email=email,name=name)
-        
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
         user.set_password(password)
-        user.save(using=self.db)
-        
+        user.save(using=self._db)
+
         return user
-    
+
     def create_superuser(self, email, name, password):
-        """Create a new superuser with given details"""
-        user=self.create_user(email, name, password)
-        
-        user.is_superuser=True
-        user.is_staff=True
-        user.save(using=self.db)
-        
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
         return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """
-    DB model for users in the system
-    """
-    
-    email       = models.EmailField(max_length=255, unique=True)
-    name        = models.CharField(max_length=255)
-    is_active   = models.BooleanField(default=True)
-    is_staff    = models.BooleanField(default=False)
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
-    
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS =['name']
-    
+    REQUIRED_FIELDS = ['name']
+
     def get_full_name(self):
-        """retrieve full name of user """
+        """Retrieve full name of user"""
         return self.name
-    
+
     def get_short_name(self):
-        """retrieve short name of user """
+        """Retrieve shot name of user"""
         return self.name
-    
+
     def __str__(self):
-        """returns string representation of our user """
+        """Return string representation of our user"""
         return self.email
